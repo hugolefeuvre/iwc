7abf5b3a247d30f31a0be1546ea1726d6dd09797
hugolefeuvre <hugolefeuvre@users.noreply.github.com>
2026-02-23 15:45:13 +0000

    deploy: 6bf48a64ccfa267ad42996b76bff5944ba2fb97a

---
+++ _astro/chunk-FMBD7UC4.DFAKrmth.js
@@ -0,0 +1,15 @@
+import{_ as o}from"./MarkdownRenderer.C7ZWi9ze.js";(function(){try{var e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{};e.SENTRY_RELEASE={id:"6bf48a64ccfa267ad42996b76bff5944ba2fb97a"};var n=new e.Error().stack;n&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[n]="ca9e2d3e-81eb-4523-9a94-8e4b7821d8ef",e._sentryDebugIdIdentifier="sentry-dbid-ca9e2d3e-81eb-4523-9a94-8e4b7821d8ef")}catch{}})();var a=o(()=>`
+  /* Font Awesome icon styling - consolidated */
+  .label-icon {
+    display: inline-block;
+    height: 1em;
+    overflow: visible;
+    vertical-align: -0.125em;
+  }
+  
+  .node .label-icon path {
+    fill: currentColor;
+    stroke: revert;
+    stroke-width: revert;
+  }
+`,"getIconStyles");export{a as g};
